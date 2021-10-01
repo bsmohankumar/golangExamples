025e63576e4a098766214dfb1c322a54b4e3aca4
MohanKumarBS <mohankumar@sidero.ie>
2021-10-01 17:22:11 +0530

    Initial golangDemo Commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "My Awesome Go App")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+}
+
+
+func main() {
+fmt.Println("Hello Welcome to Golng World!")
+	setupRoutes()
+	http.ListenAndServe(":8000", nil)
+}
